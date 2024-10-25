30502cfa98b0010e1404eea731ffc8dae25e9afe
Paradooftime <cbose344@gmail.com>
2024-10-25 21:01:46 +0530

    structure is made

---
+++ ai-docs/src/app/components/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Start from './subcomponents/start'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Start />
+
+
+      
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ ai-docs/src/app/components/subcomponents/start.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const start = () => {
+  return (
+    <div>
+      <div className='flex flex-col items-center justify-center h-screen'></div>
+    </div>
+  )
+}
+
+export default start
